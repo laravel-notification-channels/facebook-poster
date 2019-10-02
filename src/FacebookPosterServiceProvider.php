a8c3f8bd9c6aed060d2758169338936f7794d049
Dwight Watson <dwightwatson@users.noreply.github.com>
2019-10-02 16:40:51 +1000

    Improve registration in service provider

--- src/FacebookPosterServiceProvider.php
+++ src/FacebookPosterServiceProvider.php
@@ -2,31 +2,24 @@
 
 namespace NotificationChannels\FacebookPoster;
 
+use Facebook\Facebook;
 use Illuminate\Support\ServiceProvider;
 
 class FacebookPosterServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap the application services.
-     */
-    public function boot()
-    {
-        // Bootstrap code here.
-        $this->app->when(FacebookPosterChannel::class)
-            ->needs(\Facebook\Facebook::class)
-            ->give(function () {
-                return new \Facebook\Facebook([
-                    'app_id' => config('services.facebook_poster.app_id'),
-                    'app_secret' => config('services.facebook_poster.app_secret'),
-                    'default_access_token' => config('services.facebook_poster.access_token'),
-                ]);
-            });
-    }
-
-    /**
      * Register any package services.
      */
     public function register()
     {
+        $this->app->when(FacebookPosterChannel::class)
+            ->needs(Facebook::class)
+            ->give(function ($app) {
+                return new Facebook([
+                    'app_id' => $app['config']['services.facebook_poster.app_id'],
+                    'app_secret' => $app['config']['services.facebook_poster.app_secret'],
+                    'default_access_token' => $app['config']['services.facebook_poster.access_token'],
+                ]);
+            });
     }
 }
